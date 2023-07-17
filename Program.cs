1dcfcd2dea332eeacc04cade5aa4cc3a35661d88
aryark <aryark3112@gmail.com>
2023-07-17 09:18:50 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+
+namespace _17July
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Collections Continue
+
+            //ArrayList list = new ArrayList();
+            //Hashtable ht = new Hashtable();
+            //SortedList sortedList = new SortedList();
+            //Stack stack =  new Stack();
+            //Queue queue = new Queue();
+
+            //Stack example
+            Stack stack = new Stack();
+            stack.Push("Aman");
+            stack.Push("Vijay");
+            stack.Push("Zoya");
+            stack.Push("Raj");
+            stack.Push("Kiran");
+            Console.WriteLine("Number of Items in stack are: \t"+stack.Count);
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
