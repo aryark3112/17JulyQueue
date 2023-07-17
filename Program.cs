018f3692fbddfe8ccecdf9f36ce4c0511b950005
aryark <aryark3112@gmail.com>
2023-07-17 09:40:51 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -17,34 +17,63 @@ namespace _17July
             //Queue queue = new Queue();
 
             //Stack example
-            Stack stack = new Stack();
-            stack.Push("Aman");
-            stack.Push("Vijay");
-            stack.Push("Zoya");
-            stack.Push("Raj");
-            stack.Push("Kiran");
-            Console.WriteLine("Number of Items in stack are: \t"+stack.Count);
-            foreach(var item in stack)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("After removing" + stack.Pop());
-            Console.WriteLine("After removing "+ stack.Pop());
-            Console.WriteLine("Number of Items in stack are: \t"+stack.Count);
-            foreach (var item in stack)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("Peek Example: \t"+stack.Peek());
-            Console.WriteLine("After Peek");
-            Console.WriteLine("Number of Items in stack are: \t"+stack.Count);
-            foreach (var item in stack)
-            {
-                Console.WriteLine(item);
-            }
-            stack.Clear();
-            Console.WriteLine("After Clear Number of Items are: \t"+stack.Count);
+            //Stack stack = new Stack();
+            //stack.Push("Aman");
+            //stack.Push("Vijay");
+            //stack.Push("Zoya");
+            //stack.Push("Raj");
+            //stack.Push("Kiran");
+            //Console.WriteLine("Number of Items in stack are: \t"+stack.Count);
+            //foreach(var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("After removing" + stack.Pop());    //to remove an item
+            //Console.WriteLine("After removing "+ stack.Pop());
+            //Console.WriteLine("Number of Items in stack are: \t"+stack.Count);
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("Peek Example: \t"+stack.Peek());
+            //Console.WriteLine("After Peek");
+            //Console.WriteLine("Number of Items in stack are: \t"+stack.Count);
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //stack.Clear();
+            //Console.WriteLine("After Clear Number of Items are: \t"+stack.Count);
+            //Console.ReadKey();
 
+            //Queue Example
+            Queue queue = new Queue();
+            queue.Enqueue("Aman");
+            queue.Enqueue("Vijay");
+            queue.Enqueue("Zoya");
+            queue.Enqueue("Raj");
+            queue.Enqueue("Kiran");
+            Console.WriteLine("Number of Items in stack are: \t" + queue.Count);
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("After removing" + queue.Dequeue());    //to remove an item
+            Console.WriteLine("After removing " + queue.Dequeue());
+            Console.WriteLine("Number of Items in stack are: \t" + queue.Count);
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Peek Example: \t" + queue.Peek());
+            Console.WriteLine("After Peek");
+            Console.WriteLine("Number of Items in stack are: \t" + queue.Count);
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            queue.Clear();
+            Console.WriteLine("After Clear Number of Items are: \t" + queue.Count);
             Console.ReadKey();
         }
         
